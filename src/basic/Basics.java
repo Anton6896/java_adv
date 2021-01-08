7b2e37702460bcd95fe2af27dfc6a8cd6db61d66
Mac <Anton-r@live.com>
2021-01-08 12:36:47 +0500

    ff

---
+++ src/basic/Basics.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Basics {
+
+    private class Person {
+        private int age;
+        private String name;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("basic java to remember : \n");
+    }
+}
+
