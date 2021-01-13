4434060ffbd17d746eadf05a6fa6840a1c79da18
LooneyAnto_ubuntu <LooneyAnto@gmail.com>
2021-01-12 20:58:15 -0800

    gg

--- src/Design_patterns/factory_pattern/EnemyShipFactory.java
+++ src/Design_patterns/factory_pattern/EnemyShipFactory.java
@@ -2,14 +2,27 @@ package Design_patterns.factory_pattern;
 
 public class EnemyShipFactory {
 
-//    factory method that return dynamic object that depend on call
+    // factory method that return dynamic object that depend on call
     public EnemyShip makeEnemyShip(String newShipType) {
 
-        return switch (newShipType) {
-            case "U" -> new UFOEnemyShip();
-            case "R" -> new RocketEnemyship();
-            case "B" -> new BigUFOEnemyShip();
-            default -> null;
-        };
+        // return switch (newShipType) {
+        // // java 14+
+        // case "U" -> new UFOEnemyShip();
+        // case "R" -> new RocketEnemyship();
+        // case "B" -> new BigUFOEnemyShip();
+        // default -> null;
+        // };
+
+        EnemyShip ship = null;
+        switch (newShipType) {
+            case "U":
+                return new UFOEnemyShip();
+            case "R":
+                return new RocketEnemyship();
+            case "B":
+                return new BigUFOEnemyShip();
+            default:
+                return null;
+        }
     }
 }
